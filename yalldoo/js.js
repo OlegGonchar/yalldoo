cce2432d8fa4c40ffd943305b0cf9928fe679a60
oleg-gonchar <oleg.gonchar@valtech.com>
2020-05-03 02:03:56 +0300

    next commit

--- yalldoo/js.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-$("a.scroll").click(function () {
-     elementClick = $(this).attr("href");
-     destination = $(elementClick).offset().top;
-       $('html').animate( { scrollTop: destination }, 800 );
-     return false;
-   });
- });
